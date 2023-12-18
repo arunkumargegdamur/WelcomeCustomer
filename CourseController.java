464d38c0f96f238e15545d6cb70e7314d0a43275
arunmahto <154255170+arunkumargegdamur@users.noreply.github.com>
2023-12-18 23:17:57 +0530

    Add files via upload

---
+++ CourseController.java
@@ -0,0 +1,11 @@
+package com.jtcindia.springmvc;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class CourseController {
+
+	public CourseController() {
+		System.out.println("course controller");
+	}
+}
